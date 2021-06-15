f5b52b0e1ce29ea5368ec881b8cd49379e83236a
Lev Trapeznikov <leva.trapeznikov@mail.ru>
2021-06-15 14:53:45 +0300

    it's result

--- src/test/java/api/ExampleApi.java
+++ src/test/java/api/ExampleApi.java
@@ -2,6 +2,7 @@ package api;
 
 
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -12,7 +13,7 @@ import static io.restassured.RestAssured.given;
 public class ExampleApi {
     Map<String, String> cookies;
 
-    @Test
+    @BeforeEach
     void test() {
         cookies = given().accept(ContentType.JSON).when().get("https://mail.ru").getCookies();
         System.out.println(cookies);
@@ -23,6 +24,7 @@ public class ExampleApi {
         Map<String, String> map = new HashMap<>();
         map.put("login", "lev-trapeznikov@mail.ru");
         map.put("password", "Eaa4HPpOyu2%");
+        System.out.println(cookies);
 
         given().cookies(cookies).when().post("https://mail.ru").then().log().all();
 
